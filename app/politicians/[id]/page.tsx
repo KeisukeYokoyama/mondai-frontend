5ac6e482bb2b8ec2d65b9690c9be9f476da04d41
KeisukeYokoyama <gengengen71@gmail.com>
2025-03-31 17:51:06 +0900

    デプロイ調整2

--- app/politicians/[id]/page.tsx
+++ app/politicians/[id]/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PoliticianDetailClient from '@/app/politicians/[id]/PoliticianDetailClient';
 
-export default function PoliticianDetail({ params }: { params: { id: string } }) {
+type PageProps = {
+  params: { id: string };
+};
+
+export default function PoliticianDetail({ params }: PageProps) {
   return <PoliticianDetailClient id={params.id} />;
 }
