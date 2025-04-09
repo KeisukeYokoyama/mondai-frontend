e4d00bb96a37c24bf6b3988615124ce013d9d9a4
KeisukeYokoyama <gengengen71@gmail.com>
2025-04-09 16:22:38 +0900

    デプロイエラー調整

--- components/TagCloud/TopTagCloud.tsx
+++ components/TagCloud/TopTagCloud.tsx
@@ -3,8 +3,13 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase';
+import { FcSpeaker } from "react-icons/fc";
 
-export default function TopTagCloud() {
+interface TopTagCloudProps {
+  title: string;
+}
+
+export default function TopTagCloud({ title }: TopTagCloudProps) {
   const [tags, setTags] = useState<{ id: number, name: string }[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -30,25 +35,41 @@ export default function TopTagCloud() {
 
   if (loading) {
     return (
-      <div className="container px-5 py-8 mt-4 mx-auto bg-white">
-        <div className="animate-pulse flex space-x-4">
-          <div className="h-4 bg-gray-200 rounded w-24"></div>
-          <div className="h-4 bg-gray-200 rounded w-24"></div>
-          <div className="h-4 bg-gray-200 rounded w-24"></div>
+      <>
+        <div className="container px-3 pt-8 mx-auto">
+          <h2 className="text-xl font-bold text-gray-900">
+            <FcSpeaker className="inline-block mr-2" />
+            {title}
+          </h2>
         </div>
-      </div>
+        <div className="container px-5 py-8 mt-4 mx-auto bg-white">
+          <div className="animate-pulse flex space-x-4">
+            <div className="h-4 bg-gray-200 rounded w-24"></div>
+            <div className="h-4 bg-gray-200 rounded w-24"></div>
+            <div className="h-4 bg-gray-200 rounded w-24"></div>
+          </div>
+        </div>
+      </>
     );
   }
 
   return (
-    <div className="container px-5 py-8 mt-4 mx-auto bg-white flex flex-wrap">
-      {tags.map((tag) => (
-        <span key={tag.id} className="text-blue-600 font-bold mx-2 block">
-          <Link href={`/topics/${tag.id}`}>
-            #{tag.name}
-          </Link>
-        </span>
-      ))}
-    </div>
+    <>
+      <div className="container px-3 pt-8 mx-auto">
+        <h2 className="text-xl font-bold text-gray-900">
+          <FcSpeaker className="inline-block mr-2" />
+          {title}
+        </h2>
+      </div>
+      <div className="container px-5 py-8 mt-4 mx-auto bg-white">
+        {tags.map((tag) => (
+          <span key={tag.id} className="text-blue-600 font-bold mx-2">
+            <Link href={`/topics/${tag.id}`}>
+              #{tag.name}
+            </Link>
+          </span>
+        ))}
+      </div>
+    </>
   );
 }
