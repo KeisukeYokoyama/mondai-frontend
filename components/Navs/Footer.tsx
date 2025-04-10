1888db72dba5ef8b8100f6bd084f0038c039695d
KeisukeYokoyama <gengengen71@gmail.com>
2025-04-10 12:57:18 +0900

    footer 調整

--- components/Navs/Footer.tsx
+++ components/Navs/Footer.tsx
@@ -1,59 +1,64 @@
 import React from 'react';
-
+import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="text-white body-font bg-gray-800 mt-8">
-      <section className="text-white pt-10 pb-4">
-        <div className="container flex flex-col justify-center px-6 py-2 mx-auto md:p-8">
+    <div className="bg-gray-800">
+    <footer className="text-white body-font bg-gray-800 mt-8 max-w-screen-lg mx-auto">
+      <section className="text-white pt-10">
+        <div className="container flex flex-col justify-center px-6 pt-2 mx-auto md:pt-8 md:px-8 md:pb-4">
           <h2 className="mb-8 text-xl font-bold sm:text-base">よくある質問</h2>
           <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
             <details>
-              <summary className="py-2 outline-none cursor-pointer text-sm">暴言を登録するには？</summary>
+              <summary className="py-2 outline-none cursor-pointer text-sm">スクショを登録するには？</summary>
               <div className="px-4 pb-4 text-sm">
-                <p>「スクショを登録する」ボタンから登録できます。スクショの登録にはXのアカウントが必要です。</p>
+                <p>政治家/言論人詳細ページの「スクショを登録する」ボタンから登録できます。スクショの登録にはXのアカウントが必要です。</p>
               </div>
             </details>
             <details>
-              <summary className="py-2 outline-none cursor-pointer text-sm">政治家を登録するには？</summary>
+              <summary className="py-2 outline-none cursor-pointer text-sm">政治家/言論人を登録するには？</summary>
               <div className="px-4 pb-4 text-sm">
-                <p>政治家の画像は、政治家のXのプロフィール画像を使用しています。</p>
+                <p>現在、登録機能は開放していません。Xなどでアンケートを実施させていただき検討を加速します。</p>
               </div>
             </details>
             <details>
-              <summary className="py-2 outline-none cursor-pointer text-sm">虚偽の暴言があるので削除したい</summary>
+              <summary className="py-2 outline-none cursor-pointer text-sm">虚偽のスクショがあるので削除したい</summary>
               <div className="px-4 pb-4 text-sm">
-                <p>問題発言ドットコムは、XのAPIを使用しています。XのAPIを使用しているため、虚偽の暴言は登録できません。</p>
+                <p>「通報」ボタンから通報できます。独自のアルゴリズムで虚偽だと判断した場合は自動的に非表示となります。通報の多いアカウントはロックされます。
+                  また、虚偽の通報をした場合もコメント機能や通報機能が停止されます。
+                </p>
               </div>
             </details>
             <details>
               <summary className="py-2 outline-none cursor-pointer text-sm">政治家の画像を削除してほしい</summary>
               <div className="px-4 pb-4 text-sm">
-                <p>問題発言ドットコムは、XのAPIを使用しています。XのAPIを使用しているため、政治家の画像は削除できません。</p>
+                <p>X APIを使用していますので、Xのポリシーに従って対応いたします。</p>
+              </div>
+            </details>
+            <details>
+              <summary className="py-2 outline-none cursor-pointer text-sm">問題発言ドットコムとは？</summary>
+              <div className="px-4 pb-4 text-sm">
+                <p>
+                  政治家の暴言やデマ、言動不一致などのスクショを登録して、過去の暴言などを検索できるサービスです。
+                  選挙で投票する政党がない！といった迷える愛国者を救うため、消去法で選挙に参加する時の参考になれば幸いです。
+                </p>
               </div>
             </details>
           </div>
         </div>
       </section>
-      <section className="text-white pt-4 pb-10">
-        <div className="container flex flex-col justify-center px-6 py-2 mx-auto md:p-8">
-          <h2 className="mb-8 text-xl font-bold sm:text-base">問題発言ドットコムとは？</h2>
-          <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
-            <p className="text-sm">政治家の過去の暴言やデマ、言動不一致を検索できるサービスです。</p>
-          </div>
-        </div>
-      </section>
       
-      <section className="container px-5 mx-auto py-4">
+      <section className="container px-5 mx-auto md:py-6 pt-10 pb-6">
         <div className="flex flex-col items-center">
           <div className="flex space-x-4 pb-4">
-            <a href="/privacy" className="text-xs text-white hover:text-gray-300">プライバシーポリシー</a>
-            <a href="/terms" className="text-xs text-white hover:text-gray-300">利用規約</a>
+            <Link href="/privacy" className="text-xs text-white hover:text-gray-300">プライバシーポリシー</Link>
+            <Link href="/terms" className="text-xs text-white hover:text-gray-300">利用規約</Link>
           </div>
-          <p className="text-xs text-whit">
-            問題発言ドットコム
+          <p className="text-xs text-white">
+            <Link href="/">© 問題発言ドットコム</Link>
           </p>
         </div>
       </section>
     </footer>
+    </div>
   );
 }
