1dbe346f5489854b16681cf0071f42022b3bf8dc
KeisukeYokoyama <gengengen71@gmail.com>
2025-04-13 17:15:46 +0900

    parties_id_layout修正

--- app/parties/[id]/layout.tsx
+++ app/parties/[id]/layout.tsx
@@ -2,11 +2,13 @@ import type { Metadata } from 'next'
 import { partiesAPI } from '@/utils/supabase/parties';
 
 type Props = {
-  params: { id: string }
+  params: Promise<{ id: string }>;
+  children: React.ReactNode;
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { data: party } = await partiesAPI.getPartyDetail(Number(params.id));
+  const resolvedParams = await params;
+  const { data: party } = await partiesAPI.getPartyDetail(Number(resolvedParams.id));
   
   return {
     title: party ? `${party.name} | 問題発言ドットコム` : '問題発言ドットコム',
