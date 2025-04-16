c4494025edc5217ae38c7dd5623c6c4bdd626734
KeisukeYokoyama <gengengen71@gmail.com>
2025-04-16 14:21:20 +0900

    @/lib/supabas　削除

--- utils/supabase/tags.ts
+++ utils/supabase/tags.ts
@@ -1,10 +1,20 @@
-import { supabase } from '@/lib/supabase'
+import { getSupabaseClient } from './client'
 
 export const tagAPI = {
   // 一覧取得
   getAll: async () => {
-    return await supabase
+    return await getSupabaseClient()
       .from('tags')
       .select('*')
+      .order('name')
+  },
+
+  // タグの追加
+  create: async (name: string) => {
+    return await getSupabaseClient()
+      .from('tags')
+      .insert([{ name }])
+      .select()
+      .single()
   }
 }
